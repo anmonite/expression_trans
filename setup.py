238f2331155282153eda8b0208340e4c262dd68e
Norihiro Matsushita <night77@gmail.com>
2018-04-19 00:06:43 +0900

    initial comit

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='expression_trans',
+    version='0.1.0',
+    description='This package is to translate Japanese text along the expression converting model with using Sudachi as a morphological tokenizer.',
+    long_description=readme,
+    author='Norihiro Matsushita',
+    author_email='night77@gmail.com',
+    url='https://github.com/anmonite/',
+    license=license,
+    install_requires=[],
+    dependency_links=['git+ssh:github.com/WorksApplications/SudachiPy.git#egg=sudachipy'],
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
